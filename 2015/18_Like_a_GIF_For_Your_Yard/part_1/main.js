46d6a0f898b3c95219b804b2f0f8ca6370b02693
happ-2h <hikonetgames@gmail.com>
2024-06-29 14:13:26 -0700

    2015 day 18 part 01 solution

---
+++ 2015/18_Like_a_GIF_For_Your_Yard/part_1/main.js
@@ -0,0 +1,105 @@
+/**
+ * Author: happ_2h https://github.com/happ-2h
+ *
+ * Solution for Advent of Code 2015 Day 18.1
+ */
+
+let input = null;
+
+try {
+  input = require('fs').readFileSync("../input.txt", "utf-8").match(/./g);
+} catch (err) {
+  console.error(err);
+}
+
+if (input !== null) {
+  const width = 100;     // Width of given grid
+
+  let prev = [...input]; // Previous animated frame
+  let next = [...prev];  // Next animation buffer
+
+  const step = () => {
+    prev = [...next];
+
+    for (let y = 0; y < width; ++y) {
+      for (let x = 0; x < width; ++x) {
+        // Check neighbors
+        let neighbors = 0;
+
+        /*
+         * If direction is in bounds and
+         * that neighbor is on,
+         * increment "neighbors"
+         */
+        // - NW
+        if (
+          y-1 >= 0 && x-1 >= 0 &&
+          prev[(x-1)+((y-1)*width)] == '#'
+        ) ++neighbors;
+        // - N
+        if (
+          y-1 >= 0 &&
+          prev[x+((y-1)*width)] == '#'
+        ) ++neighbors;
+        // - NE
+        if (
+          y-1 >= 0 && x+1 < width &&
+          prev[(x+1)+((y-1)*width)] == '#'
+        ) ++neighbors;
+        // - W
+        if (
+          x-1 >= 0 &&
+          prev[(x-1)+y*width] == '#'
+        ) ++neighbors;
+        // - E
+        if (
+          x+1 < width &&
+          prev[(x+1)+y*width] == '#'
+        ) ++neighbors;
+        // - SW
+        if (
+          y+1 < width && x-1 >= 0 &&
+          prev[(x-1)+((y+1)*width)] == '#'
+        ) ++neighbors;
+        // - S
+        if (
+          y+1 < width &&
+          prev[x+(y+1)*width] == '#'
+        ) ++neighbors;
+        // - SE
+        if (
+          y+1 < width && x+1 < width &&
+          prev[(x+1)+((y+1)*width)] == '#'
+        ) ++neighbors;
+
+        // Check rules
+        // - If light is on and neighbors do not total 2 or 3, turn it off
+        if (prev[x+y*width] == '#' && neighbors != 2 && neighbors != 3)
+          next[x+y*width] = '.';
+        // - If light is off and has exactly 3 neighbors, turn it on
+        else if (prev[x+y*width] == '.' && neighbors == 3)
+          next[x+y*width] = '#';
+      } // End for x loop
+    } // End for y loop
+  };
+
+  // Helper function to print the array
+  const print = arr => {
+    for (let y = 0; y < width; ++y) {
+      let line = "";
+      for (let x = 0; x < width; ++x) {
+        line += arr[x+y*width];
+      }
+      console.log(line);
+    }
+    console.log();
+  };
+
+  for (let i = 0; i < 100; ++i) step();
+
+  // We only want number of lights "on"
+  const result = next.filter(char => char == '#');
+
+  console.log(result.length);
+}
+else console.error("Failed to read input.txt");
